1e1981b820323f25611f0c7f808ffa4ae60127e5
M. A Z <alfinyuki@gmail.com>
2020-08-28 20:13:01 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1, y = 1, z = 1;
+            Console.Write("Masukan jumlah bilangan fibonacci = ");
+            int a = int.Parse(Console.ReadLine());
+            if (a > 0) {
+                for (int i = 1; i <= a; i++) {
+                    Console.Write(z + " ");
+                    z = x + y;
+                    x = y;
+                    y = z;
+                }
+            }
+            else
+            {
+
+                Console.WriteLine("inputkan bilangan asli");
+               
+               
+            }
+            Console.ReadLine();
+
+            
+        }
+    }
+}
